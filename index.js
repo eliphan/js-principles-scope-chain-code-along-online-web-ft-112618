0a28f1f15106f856a53f4b84546615e70434e664
Elise Phan <aliceconnic@gmail.com>
2019-03-13 19:46:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 /* Your code goes here */
 var customerName = 'bob'
 
+var bestCustomer = 'not bob'
+
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase()
 }
+
+function 
